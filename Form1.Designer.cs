3a4af8d39a90b227aa7e75098ade56618bef88d5
Eugene <qawsed200412345@gmail.com>
2022-03-29 18:42:00 +0300

    Nu tipo bot

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Telebot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Начать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 394);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.ControlRemoved += new System.Windows.Forms.ControlEventHandler(this.listBox1_ControlRemoved);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 26;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(154, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 394);
+            this.listBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(295, 415);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(493, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(154, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Отправить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Telebot
+{
+    
+    public partial class Form1 : Form
+    {
+
+        static Random random = new Random();
+        private static bool helpfull_list_updated = false;
+        public static List<Lobby> lobbies = new List<Lobby>();
+        private static string token = "5108404020:AAE0EtaZh-Th3XoZ7KOXOVLb-8TdbUAgUvw";
+        public static TelegramBotClient client = new TelegramBotClient(token);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            client.StartReceiving();
+            client.OnMessage += MessageHandler;
+            client.OnMessage += InputLobbyId;
+            client.OnMessage += LobbyHandler;
+            PrintEveryone("Бот запущен! (✿◠‿◠)", StartButtons());
+            button1.Enabled = false;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                client.StopReceiving();
+            }
+            catch(Exception)
+            {
+            }
+        }
+        private static void AddNewUser(Telegram.Bot.Types.Chat chat)
+        {
+            StreamReader sr = new StreamReader("Users.txt");
+            if (!sr.EndOfStream)
+            {
+                string[] chats = sr.ReadToEnd().Split('\n');
+                sr.Close();
+                for (int i = 0; i < chats.Length; i++)
+                {
+                    if (long.TryParse(chats[i], out long id))
+                    {
+                        if (id == chat.Id)
+                            return;
+                    }
+                }
+            }
+            sr.Close();
+
+            using (StreamWriter sw = new StreamWriter("Users.txt", true))
+            {
+                sw.WriteLine(chat.Id);
+                sw.Close();
+                Console.WriteLine("Новый пользователь зарегистрирован!");
+            }
+        }
+        private static void PrintEveryone(string message, IReplyMarkup Buttons)
+        {
+            using (StreamReader sr = new StreamReader("Users.txt"))
+            {
+                string[] chats = sr.ReadToEnd().Split('\n');
+                for (int i = 0; i < chats.Length; i++)
+                {
+                    if (long.TryParse(chats[i], out long Id))
+                    {
+                        Telegram.Bot.Types.Chat chat = new Telegram.Bot.Types.Chat();
+                        chat.Id = Id;
+                        client.SendTextMessageAsync(chat, message, replyMarkup: Buttons); ;
+                    }
+                }
+                Console.WriteLine("Рассылка заверщена успешно!");
+            }
+        }
+        private static void PrintEveryone(string message)
+        {
+            using (StreamReader sr = new StreamReader("Users.txt"))
+            {
+                string[] chats = sr.ReadToEnd().Split('\n');
+                for (int i = 0; i < chats.Length; i++)
+                {
+                    if (long.TryParse(chats[i], out long Id))
+                    {
+                        Telegram.Bot.Types.Chat chat = new Telegram.Bot.Types.Chat();
+                        chat.Id = Id;
+                        client.SendTextMessageAsync(chat, message);
+                    }
+                }
+                Console.WriteLine("Рассылка заверщена успешно!");
+            }
+        }
+
+        public static Lobby CreateNewLobby(long id)
+        {
+            uint lobby_id = (uint)random.Next() % 89999999 + 1000000;
+            while (lobbies.Where(x => x.lobby_id == lobby_id).ToList().Count > 0)
+            {
+                lobby_id = ((uint)random.Next() + 1000000) % 100000000;
+            }
+            Lobby new_lobby = new Lobby(id, lobby_id);
+            lobbies.Add(new_lobby);
+            return new_lobby;
+        }
+
+        public static IReplyMarkup LobbyButtons()
+        {
+            return new ReplyKeyboardMarkup
+            {
+                Keyboard = new List<List<KeyboardButton>>()
+                {
+                    new List<KeyboardButton>{ new KeyboardButton{Text="delete" },new KeyboardButton { Text = "start" } , new KeyboardButton { Text = "leave" } , new KeyboardButton { Text = "info" } }
+                }
+            };
+        }
+        public static IReplyMarkup StartButtons()
+        {
+            return new ReplyKeyboardMarkup
+            {
+                Keyboard = new List<List<KeyboardButton>>()
+                {
+                    new List<KeyboardButton>{ new KeyboardButton{Text="create_lobby" },new KeyboardButton { Text = "join_lobby" } }
+                }
+            };
+        }
+
+
+        private static void MessageHandler(object sender, MessageEventArgs e)
+        {
+            var message = e.Message;
+            Console.WriteLine(message.From.FirstName + ": " + message.Text);
+            string keyword = message.Text.Split()[0];
+            AddNewUser(message.Chat);
+            switch (keyword)
+            {
+                case "/start":
+
+                    client.SendTextMessageAsync(message.Chat, "Чего желаете?", replyMarkup: StartButtons());
+                    break;
+                case "create_lobby":
+                    if (lobbies.Where(x => x.players.Where(y => y.id == message.Chat.Id).ToList().Count == 1).ToList().Count == 0)
+                    {
+                        Lobby lobby = CreateNewLobby(message.From.Id);
+                        helpfull_list_updated = true;
+                        client.SendTextMessageAsync(message.Chat, $"Лобби успешно создано\nid лобби: {lobby.lobby_id}", replyMarkup: LobbyButtons());
+                    }
+                    else
+                    {
+                        client.SendTextMessageAsync(message.Chat, "Вы уже находитесь в лобби!");
+                    }
+                    break;
+                case "join_lobby":
+                    if (lobbies.Where(x => x.players.Where(y => y.id == message.Chat.Id).ToList().Count == 1).ToList().Count == 0)
+                        client.SendTextMessageAsync(message.Chat, $"Введите id лобби");
+                    else
+                        client.SendTextMessageAsync(message.Chat, "Вы уже находитесь в лобби!");
+                    break;
+
+            }
+
+        }
+        public static void SendMessage(Lobby lobby, string message)
+        {
+            lobby.players.ForEach(x => client.SendTextMessageAsync(x.id, message));
+        }
+        public static void SendMessage(Lobby lobby, string message, IReplyMarkup Buttons)
+        {
+            lobby.players.ForEach(x => client.SendTextMessageAsync(x.id, message, replyMarkup: Buttons));
+        }
+        public static void SendMessage(long id, string message)
+        {
+            client.SendTextMessageAsync(id, message);
+        }
+        public static void SendMessage(long id, string message, IReplyMarkup Buttons)
+        {
+            client.SendTextMessageAsync(id, message, replyMarkup: Buttons);
+        }
+        private static void LobbyHandler(object sender, MessageEventArgs e)
+        {
+            var message = e.Message;
+            if (lobbies.Where(x => x.players.Where(y => y.id == message.Chat.Id).ToList().Count == 1).ToList().Count == 0) return;
+            switch (message.Text)
+            {
+                case "start":
+                    break;
+                case "leave":
+                    Lobby lobby = lobbies.Where(x => x.players.Where(y => y.id == message.Chat.Id).ToList().Count == 1).ToList()[0];
+                    if (lobby.RemovePlayer(message.Chat.Id))
+                    {
+                        client.SendTextMessageAsync(message.Chat.Id, "Вы успешно покинули лобби!", replyMarkup: StartButtons());
+                    }
+                    break;
+                case "delete":
+                    if (lobbies.Where(x => x.admin == message.Chat.Id).ToList().Count == 0) break;
+                    Lobby lobby_delete = lobbies.Where(x => x.admin == message.Chat.Id).ToList()[0];
+                    lobbies.Remove(lobby_delete);
+                    client.SendTextMessageAsync(message.Chat.Id, "Лобби успешно удалено!", replyMarkup: StartButtons());
+                    SendMessage(lobby_delete, "Лобби расформировано!", StartButtons());
+                    helpfull_list_updated = true;
+                    break;
+                case "info":
+
+                    Lobby lobby_info = lobbies.Where(x => x.players.Where(y => y.id == message.Chat.Id).ToList().Count == 1).ToList()[0];
+                    string info_message = "";
+                    info_message += "Lobby ID: " + lobby_info.lobby_id + "\n";
+                    info_message += "Lobby admin: " + client.GetChatAsync(lobby_info.admin).Result.FirstName + " (" + lobby_info.admin + ")\n\nMembers:\n";
+                    lobby_info.players.ForEach(x =>
+                    {
+                        info_message += client.GetChatAsync(x.id).Result.FirstName + " (" + x.id + ")\n";
+                    });
+                    client.SendTextMessageAsync(message.Chat.Id, info_message);
+                    break;
+            }
+        }
+
+        private static void InputLobbyId(object sender, MessageEventArgs e)
+        {
+            var message = e.Message;
+            long id;
+            if (long.TryParse(message.Text, out id))
+            {
+                if (lobbies.Where(x => x.players.Where(y => y.id == message.Chat.Id).ToList().Count == 1).ToList().Count == 0)
+                {
+                    Lobby lobby = lobbies.Find(x => x.lobby_id == id);
+                    if (lobby != null)
+                    {
+                        if (lobby.players.Where(x => x.id == message.Chat.Id).ToList().Count == 0)
+                        {
+                            lobby.AddPlayer(message.Chat.Id);
+                            Console.WriteLine($"{message.Chat.FirstName} joined lobby {lobby.lobby_id}");
+                            client.SendTextMessageAsync(message.Chat, "Вы успешно вошли в лобби!", replyMarkup: LobbyButtons());
+                        }
+                        else
+                        {
+                            client.SendTextMessageAsync(message.Chat, "Вы успешно НЕ вошли в лобби!");
+                        }
+                    }
+                    else
+                    {
+                        client.SendTextMessageAsync(message.Chat, "Вы успешно НЕ вошли в лобби!");
+                    }
+                }
+                else
+                {
+                    client.SendTextMessageAsync(message.Chat, "Вы уже находитесь в лобби!");
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            if (listBox1.SelectedIndex != -1)
+            {
+                Lobby lobby = lobbies.Where(x => x.lobby_id == uint.Parse(listBox1.SelectedItem.ToString())).ToList()[0];
+                lobby.players.ForEach(x => listBox2.Items.Add(client.GetChatAsync(x.id).Result.Id + " " + client.GetChatAsync(x.id).Result.FirstName));
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (helpfull_list_updated)
+            {
+                listBox1.Items.Clear();
+                listBox2.Items.Clear();
+                lobbies.ForEach(x =>
+                {
+                    listBox1.Items.Add(x.lobby_id);
+                });
+                helpfull_list_updated = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                Lobby lobby = lobbies.Where(x => x.lobby_id == uint.Parse(listBox1.SelectedItem.ToString())).ToList()[0];
+                if (listBox2.SelectedIndex != -1)
+                {
+                    Player player = lobby.players.Where(x => x.id == long.Parse(listBox2.SelectedItem.ToString().Split()[0])).ToList()[0];
+                    client.SendTextMessageAsync(client.GetChatAsync(player.id).Result.Id, textBox1.Text);
+                    return;
+                }
+                lobby.players.ForEach(x => client.SendTextMessageAsync(client.GetChatAsync(x.id).Result.Id, textBox1.Text));
+            }
+            else
+            {
+                PrintEveryone(textBox1.Text);
+            }
+        }
+
+        private void listBox1_ControlRemoved(object sender, ControlEventArgs e)
+        {
+        }
+
+        
+    }
+    public class Player
+    {
+        public long id;
+
+
+        public Player(long id)
+        {
+            this.id = id;
+        }
+    }
+    public class Shelter
+    {
+
+    }
+
+    public class Disaster
+    {
+
+    }
+    public struct GameStage
+    {
+
+    }
+    public class Lobby
+    {
+        Shelter shelter;
+        Disaster disaster;
+        public long admin;
+        public uint lobby_id;
+        public List<Player> players = new List<Player>();
+
+        public Lobby(long admin, uint id)
+        {
+            this.admin = admin;
+            lobby_id = id;
+            players.Add(new Player(admin));
+        }
+
+        public void AddPlayer(long id)
+        {
+            players.Add(new Player(id));
+        }
+
+        public bool RemovePlayer(long id)
+        {
+            return id != admin ? players.Remove(players.Where(x => x.id == id).ToList()[0]) : false;
+        }
+    }
+}
